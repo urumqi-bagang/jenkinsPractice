5491fd8102ba4ec4e36e686b1a07d5f6598e131e
Dawud_SF <dawudabduweli@gmail.com>
2020-12-21 22:21:45 -0800

    @Before @After from cucumber library

--- src/test/java/com/dec14/stepDefinitions/Hooks.java
+++ src/test/java/com/dec14/stepDefinitions/Hooks.java
@@ -1,10 +1,10 @@
 package com.dec14.stepDefinitions;
 
 
-import com.dec14.utils.Driver;
-import org.junit.After;
 
-import org.junit.Before;
+import com.dec14.utils.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 
 
 import java.util.concurrent.TimeUnit;
@@ -12,12 +12,14 @@ import java.util.concurrent.TimeUnit;
 public class Hooks {
     @Before
     public void setUp() {
+        System.out.println("Test started!!!");
         Driver.getDriver().manage().window().maximize();
         Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @After
     public void beforeEachTest() {
+        System.out.println("Test end!!!");
         Driver.closeDriver();
     }
 }
