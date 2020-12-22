f4d4cc36d10fa7079d326a7c8886b8206c30d2da
Dawud_SF <dawudabduweli@gmail.com>
2020-12-21 22:11:24 -0800

    changed the before/after class to before/after

--- src/test/java/com/dec14/stepDefinitions/Hooks.java
+++ src/test/java/com/dec14/stepDefinitions/Hooks.java
@@ -2,19 +2,21 @@ package com.dec14.stepDefinitions;
 
 
 import com.dec14.utils.Driver;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+
+import org.junit.Before;
+
 
 import java.util.concurrent.TimeUnit;
 
 public class Hooks {
-    @BeforeClass
+    @Before
     public void setUp() {
         Driver.getDriver().manage().window().maximize();
         Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-    @AfterClass
+    @After
     public void beforeEachTest() {
         Driver.getDriver().quit();
     }
